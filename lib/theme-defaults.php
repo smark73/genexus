ef1c60612567c0d74d8d2c2aaae1380a75361764
Stacy <stacy@stacymark.com>
2017-11-29 08:35:35 -0800

    mv to Windows WSL - rebuild files after reinstall node, npm, gulp

--- lib/theme-defaults.php
+++ lib/theme-defaults.php
@@ -19,7 +19,8 @@ function genesis_sample_theme_defaults( $defaults ) {
 	$defaults['content_archive_limit']     = 0;
 	$defaults['content_archive_thumbnail'] = 0;
 	$defaults['posts_nav']                 = 'numeric';
-	$defaults['site_layout']               = 'content-sidebar';
+	$defaults['site_layout']               = 'full-width-content';
+	//$defaults['site_layout']               = 'content-sidebar';
 
 	return $defaults;
 
@@ -37,7 +38,8 @@ function genesis_sample_theme_setting_defaults() {
 			'content_archive_limit'     => 0,
 			'content_archive_thumbnail' => 0,
 			'posts_nav'                 => 'numeric',
-			'site_layout'               => 'content-sidebar',
+			'site_layout'               => 'full-width-content',
+			//'site_layout'               => 'content-sidebar',
 		) );
 
 	}
